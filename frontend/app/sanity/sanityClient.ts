d943b0ebf9491e37171217780afc153ec839d9c8
TLTeechbender <myjoyhasarrived2004@gmail.com>
2025-07-27 08:59:59 +0100

    update: moved files around

--- frontend/app/sanity/sanityClient.ts
+++ frontend/app/sanity/sanityClient.ts
@@ -1,6 +1,5 @@
-import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
-
+import { createClient } from "@sanity/client";
 export const sanityClient = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID!,
   dataset: import.meta.env.VITE_SANITY_DATASET!,
